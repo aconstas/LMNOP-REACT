817ba697446ac111ce6b8b4d1f4120f9f433bf13
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-09 19:48:43 -0800

    render <GuessInput /> in Game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,5 +1,18 @@
-// export default function Game() {
-//     return (
+import GuessInput from "../GuessInput/GuessInput";
+import Timer from "../Timer/Timer";
+import styles from "./Game.module.css";
 
-//     );
-// }
+export default function Game({ isModalOpen }) {
+  return (
+    <div className={isModalOpen ? styles.blurred : ""}>
+      <Timer />
+      <div id={styles.guessInputContainer}>
+        <GuessInput />
+        <GuessInput />
+        <GuessInput />
+        <GuessInput />
+        <GuessInput />
+      </div>
+    </div>
+  );
+}
