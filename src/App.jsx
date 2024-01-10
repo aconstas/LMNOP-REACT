4a6851e1f2bdf88f4a73fce840e155d21d2fd07e
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-09 19:48:21 -0800

    import HowTo, Game, Results into App

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react';
 import './App.css'
 import Howto from './components/HowTo/Howto';
 import Navbar from './components/Navbar/Navbar';
+import Results from './components/Results/Results';
+import Game from './components/Game/Game';
 
 function App() {
   const [showResults, setshowResults] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const toggleModal = () => {
+    setIsModalOpen(false);
+  }
 
   return (
     <>
       <Navbar />
-      < Howto />
+      {isModalOpen && < Howto closeModal={toggleModal} isModalOpen={isModalOpen}/>}
+      <Game isModalOpen={isModalOpen}/>
+      {showResults && <Results />}
     </>
   )
 }
