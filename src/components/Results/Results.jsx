93d3abf8eb5eb9acc09568c9e6cdc8eedd38de9b
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-10 11:52:59 -0800

    add temporary placeholder values for Results modal

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,5 +1,33 @@
-import styles from '../Howto/Howto.module.css';
+import styles from "../../shared/styles/modal.module.css";
 
+// should accept result object as props
 export default function Results() {
-    return('hello');
+  return (
+    <>
+      <div className={styles.modalContainer}>
+        <h2 className={styles.modalTitle}>RESULTS</h2>
+        <div>
+          <h2 className={styles.modalHeading}>1:25</h2>
+          <div className={styles.resultsBoard}></div>
+          <div id={styles.stats}>
+            <div>
+              <h3 className={styles.modalHeading}>5</h3>
+              <p className={styles.statsDescription}>played</p>
+            </div>
+            <div>
+              <h3 className={styles.modalHeading}>87%</h3>
+              <p className={styles.statsDescription}>accuracy</p>
+            </div>
+            <div>
+              <h3 className={styles.modalHeading}>1</h3>
+              <p className={styles.statsDescription}>streak</p>
+            </div>
+          </div>
+        </div>
+        <button className={styles.modalButton}>SHARE</button>
+      </div>
+    </>
+  );
 }
+
+// .modalText
