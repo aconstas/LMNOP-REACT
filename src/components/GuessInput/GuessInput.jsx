c251384f1e64d42b046fe41ef9b66a86bb261815
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-10 21:50:26 -0800

    change: rename class for clarity

--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -2,7 +2,7 @@ import styles from "./GuessInput.module.css";
 
 export default function GuessInput() {
   return (
-    <div className={styles.letterContainer}>
+    <div className={styles.container}>
       <div className={styles.letterBox}></div>
       <div>
         <input type="text" required enterKeyHint="go" autoComplete="off"></input>
