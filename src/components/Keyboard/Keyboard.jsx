c9291133ef4be2dcc842322c0d04d1d8c81e3b26
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-13 12:57:32 -0800

    style: reduce timeout key background color change on click

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -10,7 +10,7 @@ export default function Keyboard({ isModalOpen }) {
 
     setTimeout(() => {
       setActiveKey(null);
-    }, 25);
+    }, 10);
   };
 
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
