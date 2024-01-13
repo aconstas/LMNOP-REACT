70bb742c32112b7fe22a212306203a7e0c259877
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-12 19:44:13 -0800

    accept isModalOpen to apply blurred overlay

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,13 +1,13 @@
 import styles from "./Keyboard.module.css";
 import backspace from "../../assets/backspace.png";
 
-export default function Keyboard() {
+export default function Keyboard({ isModalOpen }) {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
   return (
-    <div id={styles.keyboard}>
+    <div id={styles.keyboard} className={isModalOpen ? styles.blurred : null}>
       <div className={styles.line1}>
         {keys1.map((key) => {
           return <div key={key} className={styles.key}>{key}</div>;
