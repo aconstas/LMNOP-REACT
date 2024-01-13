3ce9c8c14c41956b8ed3d848dbc3e3c6dffdb33b
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-12 19:43:19 -0800

    refactor: render <CurrentHint />

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,3 +1,4 @@
+import CurrentHint from "../CurrentHint/CurrentHint";
 import GuessInput from "../GuessInput/GuessInput";
 import Timer from "../Timer/Timer";
 import styles from "./Game.module.css";
@@ -5,6 +6,7 @@ import styles from "./Game.module.css";
 export default function Game({ isModalOpen }) {
   return (
     <div className={isModalOpen ? styles.blurred : ""}>
+      <CurrentHint />
       <div id={styles.guessInputContainer}>
         <GuessInput />
         <GuessInput />
