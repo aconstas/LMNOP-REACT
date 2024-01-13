90df536c6cd29068f2f02256b2c04e3090fe2711
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-13 11:50:19 -0800

    style: add backgroundColor change on key click

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,7 +1,18 @@
 import styles from "./Keyboard.module.css";
 import backspace from "../../assets/backspace.png";
+import { useState } from "react";
 
 export default function Keyboard({ isModalOpen }) {
+  const [activeKey, setActiveKey] = useState(null);
+
+  const handleKeyClick = (key) => {
+    setActiveKey(key);
+
+    setTimeout(() => {
+      setActiveKey(null);
+    }, 100);
+  };
+
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
@@ -10,20 +21,49 @@ export default function Keyboard({ isModalOpen }) {
     <div id={styles.keyboard} className={isModalOpen ? styles.blurred : null}>
       <div className={styles.line1}>
         {keys1.map((key) => {
-          return <div key={key} className={styles.key}>{key}</div>;
+          return (
+            <div
+              key={key}
+              className={styles.key}
+              onClick={() => handleKeyClick(key)}
+              style={{ backgroundColor: activeKey === key && "#E5E5E5" }}
+            >
+              {key}
+            </div>
+          );
         })}
       </div>
       <div className={styles.line2}>
         {keys2.map((key) => {
-          return <div key={key} className={styles.key}>{key}</div>;
+          return (
+            <div
+              key={key}
+              className={styles.key}
+              onClick={() => handleKeyClick(key)}
+              style={{ backgroundColor: activeKey === key && "#E5E5E5" }}
+            >
+              {key}
+            </div>
+          );
         })}
       </div>
       <div className={styles.line3}>
         <div id={styles.enterKey}>ENTER</div>
         {keys3.map((key) => {
-          return <div key={key} className={styles.key}>{key}</div>;
+          return (
+            <div
+              key={key}
+              className={styles.key}
+              onClick={() => handleKeyClick(key)}
+              style={{ backgroundColor: activeKey === key && "#E5E5E5" }}
+            >
+              {key}
+            </div>
+          );
         })}
-        <div id={styles.backspaceKey}><img src={backspace}/></div>
+        <div id={styles.backspaceKey}>
+          <img src={backspace} />
+        </div>
       </div>
     </div>
   );
