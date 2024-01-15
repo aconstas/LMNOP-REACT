d971bf6e43eeaab4e2c8b270cf7d3da6d1e98941
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-14 21:59:44 -0800

    build: set state in <Game /> to handle game start + end

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import CurrentHint from "../CurrentHint/CurrentHint";
 import GuessInput from "../GuessInput/GuessInput";
 import Timer from "../Timer/Timer";
 import styles from "./Game.module.css";
 
 export default function Game({ isModalOpen }) {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameEnded, setGameEnded] = useState(false);
+
+
   return (
     <div className={isModalOpen ? styles.blurred : ""}>
       <CurrentHint />
@@ -18,3 +23,6 @@ export default function Game({ isModalOpen }) {
     </div>
   );
 }
+
+// i think i will need to utilize the data and map over the word list to render the guess inputs
+// GuessInput will need to have correctWord state, and guessText
