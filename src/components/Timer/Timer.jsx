870b5f3d15d03d72012f5d392f03fa7384301807
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-15 13:50:59 -0800

    build: timer start functionality on gameStarted

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,7 +1,27 @@
-import styles from './Timer.module.css';
+import styles from "./Timer.module.css";
+import { useEffect, useState } from "react";
 
-export default function Timer() {
-    return (
-        <h5 id={styles.timer}>0:00</h5>
-    );
+export default function Timer({ gameStarted }) {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (gameStarted) {
+        interval = setInterval(() => {
+            setTime(prevTime => prevTime + 1);
+        }, 1000);
+    } else if (!gameStarted && time !== 0) {
+        setTime(0);
+    }
+    return () => clearInterval(interval);
+  }, [gameStarted, time]);
+
+  const formatTime = (totalSeconds) => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  return <h5 id={styles.timer}>{formatTime(time)}</h5>;
 }
