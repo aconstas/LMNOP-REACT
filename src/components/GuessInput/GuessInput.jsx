71c1b39fde369c40968ecff2cb85142e7a9013ab
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-16 13:22:21 -0800

    test: add placeholder letter

--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -3,7 +3,7 @@ import styles from "./GuessInput.module.css";
 export default function GuessInput() {
   return (
     <div className={styles.container}>
-      <div className={styles.letterBox}></div>
+      <div className={styles.letterBox}>L</div>
       <input type="text" enterKeyHint="go" autoComplete="off" readOnly></input>
     </div>
   );
