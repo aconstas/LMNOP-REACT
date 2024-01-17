86c5d4788d0a6c317b74a692ad9c7be38060b06b
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-16 20:03:11 -0800

    fix: update instructions

--- src/components/Howto/Howto.jsx
+++ src/components/Howto/Howto.jsx
@@ -12,7 +12,7 @@ export default function Howto({ closeModal, isModalOpen }) {
         <p className={styles.modalText}>
           Using 1-word clues, guess 5 words that start with each of the given
           alphabetical letters. You have 3 chances for each letter. The clues
-          can be synonyms, antonyms, etc.
+          can be synonyms, antonyms, part to whole, or any other word relationship.
         </p>
         <button className={styles.modalButton} onClick={closeModal}>
           LET'S PLAY!
