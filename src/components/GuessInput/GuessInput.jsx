6d8e6cac4fa427f4112cb005ba8fc57f3077218c
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-18 12:07:47 -0800

    style: render first letter of word in letter box

--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -1,9 +1,9 @@
 import styles from "./GuessInput.module.css";
 
-export default function GuessInput({ userGuess }) {
+export default function GuessInput({ userGuess, correctWord }) {
   return (
     <div className={styles.container}>
-      <div className={styles.letterBox}>L</div>
+      <div className={styles.letterBox}>{correctWord[0].toUpperCase()}</div>
       <input type="text" enterKeyHint="go" autoComplete="off" readOnly value={userGuess}></input>
     </div>
   );
