d1f1c55ccf14f7261761f3b99e2b5fe2dde9c5e9
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-18 12:06:50 -0800

    test: calc daysSinceLaunch and retrieve wordlist

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import Results from "./components/Results/Results.jsx";
 import Game from "./components/Game/Game.jsx";
 import Keyboard from "./components/Keyboard/Keyboard.jsx";
+import wordlists from './constants/wordlists.json';
 
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [gameStarted, setGameStarted] = useState(false);
   const [gameEnded, setGameEnded] = useState(false);
   const [userGuess, setUserGuess] = useState("");
+
+  const launchDate = new Date('2024-01-18');
+  const currentDate = new Date();
+  const daysSinceLaunch = Math.floor((currentDate - launchDate) / (1000 * 60 * 60 * 24));
+  const currentGame = wordlists.filter(game => game.days_since_launch === daysSinceLaunch);
+  const currentWordlist = currentGame[0].wordlist;
 
   function addUserText(key) {
     const newText = userGuess + key;
@@ -37,7 +44,7 @@ export default function App() {
         <Howto closeModal={toggleModal} isModalOpen={isModalOpen} />
       )}
       {gameEnded && <Results />}
-      <Game isModalOpen={isModalOpen} gameStarted={gameStarted} userGuess={userGuess}/>
+      <Game isModalOpen={isModalOpen} gameStarted={gameStarted} userGuess={userGuess} currentWordlist={currentWordlist}/>
       <Keyboard isModalOpen={isModalOpen} addUserText={addUserText} handleBackspace={handleBackspace}/>
     </>
   );
