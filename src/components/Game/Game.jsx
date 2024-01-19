80e1fc855a815481096c3f66fe644ab5d0e9d369
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-18 22:34:27 -0800

    test: pass shakeIncorrect to GuessInput

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -12,6 +12,7 @@ export default function Game({
   activeInputIndex,
   checkGuess,
   hints,
+  shakeIncorrect,
 }) {
 
   return (
@@ -26,6 +27,7 @@ export default function Game({
               correctWord={wordSet.word}
               isActive={index === activeInputIndex}
               checkGuess={checkGuess}
+              shakeIncorrect={shakeIncorrect}
             />
           );
         })}
