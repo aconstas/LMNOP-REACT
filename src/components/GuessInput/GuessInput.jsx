5996b2555af87ecf39fb9cf227401c75ebda6fe4
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-18 16:28:24 -0800

    use isActive + useRef to determine which input should be active

--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -1,10 +1,34 @@
+import { useRef, useEffect } from "react";
 import styles from "./GuessInput.module.css";
 
-export default function GuessInput({ userGuess, correctWord }) {
+export default function GuessInput({
+  userGuess,
+  correctWord,
+  isActive,
+  checkGuess,
+}) {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (isActive) {
+      inputRef.current?.focus();
+    }
+  }, [isActive]);
+
   return (
     <div className={styles.container}>
-      <div className={styles.letterBox}>{correctWord[0].toUpperCase()}</div>
-      <input type="text" enterKeyHint="go" autoComplete="off" readOnly value={userGuess}></input>
+      <div className={`${styles.letterBox} ${isActive ? styles.active : ""}`}>
+        {correctWord[0].toUpperCase()}
+      </div>
+      <input
+        ref={inputRef}
+        type="text"
+        enterKeyHint="go"
+        autoComplete="off"
+        readOnly
+        value={isActive ? userGuess : ""}
+        className={isActive && styles.active}
+      ></input>
     </div>
   );
 }
