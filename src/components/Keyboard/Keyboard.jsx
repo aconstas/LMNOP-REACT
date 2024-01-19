180ffa55d9fbeaca0b1be3be25ea502bbe35fc8a
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-18 16:21:49 -0800

    feat: checkGuess on handleEnter

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -3,10 +3,15 @@ import backspace from "../../assets/backspace.png";
 import Key from "../Key/Key";
 import { useState } from "react";
 
-export default function Keyboard({ isModalOpen, addUserText, handleBackspace }) {
+export default function Keyboard({ isModalOpen, addUserText, handleBackspace, userGuess, checkGuess, activeGuess, correctWord }) {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  const handleEnter = () => {
+    console.log(activeGuess, correctWord);
+    checkGuess(activeGuess, correctWord);
+  }
 
   return (
     <div id={styles.keyboard} className={isModalOpen ? styles.blurred : null}>
@@ -23,7 +28,7 @@ export default function Keyboard({ isModalOpen, addUserText, handleBackspace }) 
         })}
       </div>
       <div className={styles.line3}>
-        <div id={styles.enterKey}>ENTER</div>
+        <div id={styles.enterKey} onClick={handleEnter}>ENTER</div>
         {keys3.map((key) => {
           return <Key keyVal={key} key={key} addUserText={() => addUserText(key)} />;
         })}
