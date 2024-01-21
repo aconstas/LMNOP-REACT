5f636ea66db255f4ed2dfd3b449239aa650ce9e5
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-20 16:11:35 -0800

    fix: conditionally render lastWordHint when activeInputIndex is null at the end of game

--- src/components/CurrentHint/CurrentHint.jsx
+++ src/components/CurrentHint/CurrentHint.jsx
@@ -1,5 +1,6 @@
 import styles from "./CurrentHint.module.css";
 
 export default function CurrentHint({ hints, activeInputIndex }) {
-  return <h5 id={styles.hint}>{hints[activeInputIndex]}</h5>;
+  const lastWordHint = hints[4];
+  return <h5 id={styles.hint}>{hints[activeInputIndex] || lastWordHint}</h5>;
 }
