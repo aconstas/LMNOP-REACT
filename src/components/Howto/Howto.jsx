b141981626ad30c1f59d3cd18b6e5ec13639a79b
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-28 16:58:37 -0800

    style: add div for modal backgroud blur, import useStopwatch context

--- src/components/Howto/Howto.jsx
+++ src/components/Howto/Howto.jsx
@@ -1,12 +1,21 @@
 import styles from '../../shared/styles/modal.module.css';
+import { useStopwatch } from '../../contexts/stopwatchContext';
 
 export default function Howto({ closeModal, isModalOpen }) {
+  console.log('how-to rendered')
+  const {isRunning, setIsRunning} = useStopwatch();
   if (!isModalOpen) {
     return null;
   }
 
+  const start = () => {
+    setIsRunning(true);
+    closeModal();
+  }
+
   return (
     <>
+      <div className={styles.modalBackground}></div>
       <div className={styles.modalContainer}>
         <h2 className={styles.modalTitle}>HOW-TO:</h2>
         <p className={styles.modalText}>
@@ -14,7 +23,7 @@ export default function Howto({ closeModal, isModalOpen }) {
           alphabetical letters. You have 3 chances for each letter. The clues
           can be synonyms, antonyms, part to whole, or any other word relationship.
         </p>
-        <button className={styles.modalButton} onClick={closeModal}>
+        <button className={styles.modalButton} onClick={start}>
           LET'S PLAY!
         </button>
       </div>
