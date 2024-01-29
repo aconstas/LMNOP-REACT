19335f7571c96f590b3f0cea76969c6f58e4128c
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-28 16:53:45 -0800

    useStopwatch context

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,18 +1,21 @@
 import styles from "./Timer.module.css";
-import { useEffect } from "react";
+import { useStopwatch } from "../../contexts/stopwatchContext";
 
-export default function Timer({ gameStarted, time, setTime }) {
+export default function Timer({gameStarted}) {
+  const {time, isRunning, setIsRunning} = useStopwatch();
+  if (!gameStarted) {
+    setIsRunning(false);
+  }
+  // useEffect(() => {
+  //   let interval;
 
-  useEffect(() => {
-    let interval;
-
-    if (gameStarted) {
-        interval = setInterval(() => {
-            setTime(prevTime => prevTime + 1);
-        }, 1000);
-    } 
-    return () => clearInterval(interval);
-  }, [gameStarted, time]);
+  //   if (gameStarted) {
+  //       interval = setInterval(() => {
+  //           setTime(prevTime => prevTime + 1);
+  //       }, 1000);
+  //   } 
+  //   return () => clearInterval(interval);
+  // }, [gameStarted, time]);
 
   const formatTime = (totalSeconds) => {
     const minutes = Math.floor(totalSeconds / 60);
