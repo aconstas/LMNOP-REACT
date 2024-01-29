528122d948fb73a631eb3065f81b361091002013
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-28 16:57:42 -0800

    remove unused activeKey logic. place handleKeyClick in useCallback

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,24 +1,16 @@
+import { useCallback } from "react";
 import styles from "./Key.module.css";
-// import { useState } from "react";
 
 export default function Key({ keyVal, addUserText }) {
-  // const [activeKey, setActiveKey] = useState(false);
 
-  const handleKeyClick = (key) => {
+  const handleKeyClick = useCallback((key) => {
     addUserText(key)
-
-    // setActiveKey(!activeKey);
-
-    // setTimeout(() => {
-    //   setActiveKey(false);
-    // }, 20);
-  };
+  }, [addUserText]);
 
   return (
     <div
       className={styles.key}
       onClick={() => handleKeyClick()}
-      // style={{ backgroundColor: activeKey && "#E5E5E5" }}
     >
       {keyVal}
     </div>
