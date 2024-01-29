e970fd46c426cf1859ddee134389e2a63f9eb9b9
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-28 17:53:44 -0800

    refactor inline handleClick function

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,18 +1,21 @@
 import { useCallback } from "react";
 import styles from "./Key.module.css";
+import React from "react";
 
-export default function Key({ keyVal, addUserText }) {
+const Key = ({ keyVal, addUserText }) => {
 
-  const handleKeyClick = useCallback((key) => {
-    addUserText(key)
+  const handleKeyClick = useCallback((keyVal) => {
+    addUserText(keyVal)
   }, [addUserText]);
 
   return (
     <div
       className={styles.key}
-      onClick={() => handleKeyClick()}
+      onClick={handleKeyClick}
     >
       {keyVal}
     </div>
   );
 }
+
+export default React.memo(Key);
