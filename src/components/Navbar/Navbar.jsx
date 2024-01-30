d89575a75a063ac7322da8c4f350d22aa365ff3f
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-29 16:05:27 -0800

    add props to toggle <Instructions> modal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,18 @@ import styles from './Navbar.module.css';
 import settings from '../../assets/settings.png';
 import help from '../../assets/help.png';
 
-export default function Navbar() {
+export default function Navbar({showInstructions, setShowInstructions}) {
     console.log('navbar re-rendered');
+    const toggleInstructions = () => {
+        console.log('clicked!')
+        setShowInstructions(!showInstructions);
+    }
     return (
         <header id={styles.header}>
             <h1 id={styles.title}>LMNOP</h1>
             <div id={styles.buttons}>
                 <img src={settings} className={styles.icons} alt='settings icon'/>
-                <img src={help} className={styles.icons} alt='help icon'/>
+                <img src={help} className={styles.icons} onClick={toggleInstructions} alt='instructions icon'/>
             </div>
         </header>
     );
