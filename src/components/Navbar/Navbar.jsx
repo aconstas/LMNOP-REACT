86cac954cef7b62b35a92ed0f4926c4116a1dcec
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-30 17:19:25 -0800

    pass in showSettings state to toggle <Settings>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import styles from './Navbar.module.css';
 import settings from '../../assets/settings.png';
 import help from '../../assets/help.png';
 
-export default function Navbar({showInstructions, setShowInstructions}) {
+export default function Navbar({showInstructions, setShowInstructions, showSettings, setShowSettings}) {
     console.log('navbar re-rendered');
+    const toggleSettings = () => {
+        setShowSettings(!showSettings);
+    }
     const toggleInstructions = () => {
         console.log('clicked!')
         setShowInstructions(!showInstructions);
@@ -12,7 +15,7 @@ export default function Navbar({showInstructions, setShowInstructions}) {
         <header id={styles.header} className={showInstructions ? styles.blurred : undefined}>
             <h1 id={styles.title}>LMNOP</h1>
             <div id={styles.buttons}>
-                <img src={settings} className={styles.icons} alt='settings icon'/>
+                <img src={settings} className={styles.icons} onClick={toggleSettings} alt='settings icon'/>
                 <img src={help} className={styles.icons} onClick={toggleInstructions} alt='instructions icon'/>
             </div>
         </header>
