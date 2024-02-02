28af7619a6e0a1c79d6af2cdb5a2e92f5938c34d
aconstas <82304192+aconstas@users.noreply.github.com>
2024-02-02 09:47:42 -0800

    style: display null if lastPlayed === currentDate

--- src/components/CurrentHint/CurrentHint.jsx
+++ src/components/CurrentHint/CurrentHint.jsx
@@ -1,6 +1,13 @@
 import styles from "./CurrentHint.module.css";
 
-export default function CurrentHint({ hints, activeInputIndex }) {
+export default function CurrentHint({ hints, activeInputIndex, lastPlayed, currentDate }) {
+
+  if (lastPlayed === currentDate) {
+    return (
+      <div style={{margin: "20px"}}></div>
+    )
+  }
+
   const lastWordHint = hints[4];
   return <h5 id={styles.hint}>{hints[activeInputIndex] || lastWordHint}</h5>;
 }
