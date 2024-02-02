fc61aeb82d14902dd335f3438f519ac57f95d6d5
aconstas <82304192+aconstas@users.noreply.github.com>
2024-02-02 09:46:20 -0800

    feat: display lastGameTime if user has already played from localStorage

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,8 +1,9 @@
 import styles from "./Timer.module.css";
 import { useStopwatch } from "../../contexts/stopwatchContext";
 import { useEffect } from "react";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
-export default function Timer({gameStarted}) {
+export default function Timer({gameStarted, lastPlayed, currentDate}) {
   const {time, isRunning, setIsRunning} = useStopwatch();
 
   useEffect(() => {
@@ -12,11 +13,16 @@ export default function Timer({gameStarted}) {
       setIsRunning(true);
     }
   }, [gameStarted, setIsRunning]);
-
+  
   const formatTime = (totalSeconds) => {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
     return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  if (lastPlayed === currentDate) {
+    const [lastTime] = useLocalStorage('lastGameTime')
+    return <h5 id={styles.timer}>{formatTime(lastTime)}</h5>;
   }
 
   return <h5 id={styles.timer}>{formatTime(time)}</h5>;
