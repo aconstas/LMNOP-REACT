55c35865a5600f335c38c1d5b29b215550583783
aconstas <82304192+aconstas@users.noreply.github.com>
2024-06-02 21:34:36 -0700

    createBrowserRouter
    Users get routed to the Construction page when there are no wordlists left.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import Construction from '../pages/construction.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/uh-oh",
+    element: <Construction/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
