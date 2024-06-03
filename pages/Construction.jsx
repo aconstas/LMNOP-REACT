43dfd04ae5d132b6f1010d1c3c672f464cbf6790
aconstas <82304192+aconstas@users.noreply.github.com>
2024-06-02 21:33:30 -0700

    initial commit

---
+++ pages/Construction.jsx
@@ -0,0 +1,24 @@
+export default function Construction() {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: '2rem' }}
+    >
+      <div>
+        <h1>We're sorry, LMNOP is currently unavailable.</h1>
+        <h1>We'll get it right back up!</h1>
+      </div>
+      <iframe
+        src="https://giphy.com/embed/EIiJp9cQ3GeEU"
+        width="480"
+        height="480"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/gifs/please-construction-patient-EIiJp9cQ3GeEU">
+          via GIPHY
+        </a>
+      </p>
+    </div>
+  );
+}
